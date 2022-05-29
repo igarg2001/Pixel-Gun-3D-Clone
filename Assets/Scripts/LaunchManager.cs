7625a2457c67db72031a90eabc93d7d3aff02411
igarg2001 <igarg2001@gmail.com>
2022-05-29 23:10:18 +0530

    add files

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+
+    public GameObject EnterGamePanel;
+    public GameObject ConnectionStatusPanel;
+
+    #region Unity Methods
+    void Start()
+    {
+        //PhotonNetwork.ConnectUsingSettings();
+        EnterGamePanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Public Methods
+    public void ConnectToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            ConnectionStatusPanel.SetActive(true);
+            EnterGamePanel.SetActive(false);
+        }
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.NickName + " is Connected to Photon Server");
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet");
+    }
+
+
+
+    #endregion
+
+}
---
+++ Assets/Scripts/PlayerNameInputManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerNameInputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void SetPlayerName(string playerName)
+    {
+        if(string.IsNullOrEmpty(playerName))
+        {
+            Debug.Log("Player name is empty");
+            return;
+        }
+        PhotonNetwork.NickName = playerName;
+    }
+
+    
+}
